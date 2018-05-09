c57a367c093b2c295555d03b466d389e91066e85
mjlavin80 <lavin@pitt.edu>
2018-05-09 08:09:08 -0400

    prep

---
+++ prep_25k.py
@@ -0,0 +1,35 @@
+import glob, json
+from elasticsearch import Elasticsearch
+
+def prep_25_elastic(folder):
+    
+    print("reading json")
+    try: 
+        with open("%s.json" % folder) as c:
+            myjson = c.readlines()
+        print("found bulkfile")
+    except:
+        myfiles = glob.glob("/aps/aps_get/json/%s/*.json" % folder)
+        myjson = []
+        for f in myfiles:                                                
+           with open(f) as j:
+               jsontxt = j.read()
+               myjson.append(jsontxt)
+
+        print("writing bulkfile")
+        with open("%s.json" % folder, "a") as c: 
+           for m in myjson:
+               c.write(m)
+               c.write("\n")
+        c.close() 
+
+    
+if __name__ == '__main__':
+    folders = []
+    for root, dirs, files in os.walk("/aps/aps_get/json"):
+        folders.extend(dirs)
+        break
+
+    for e, f in enumerate(folders)[:46]:
+        print(e, "out of ", len(folders))
+        prep_25_elastic(folder)
