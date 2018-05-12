0677f43b253cb61a1f10d0b2c4684e26e6e5e390
mjlavin80 <lavin@pitt.edu>
2018-05-12 09:33:06 -0400

    test elastic

--- test_insert.py
+++ test_insert.py
@@ -10,25 +10,30 @@ for root, dirs, files in os.walk("/aps/aps_get/json"):
     folders.extend(dirs)
     break
 
-#open bulk file for folder no. 262
+data = []
+#loop bulk folders
+for f in folders:
+    with open("%s.json" % f) as c:
+        myjson = c.readlines()
 
-with open("%s.json" % folders[263]) as c:
-    myjson = c.readlines()
+    bulk_ids = []
 
-bulk_ids = []
+    for i in myjson:
+        data_dict = json.loads(i)
+        # get approx 25k ids using the data_dict['RecordID']
+        bulk_ids.append(data_dict['RecordID'])
 
-for i in myjson:
-    data_dict = json.loads(i)
-    # get approx 25k ids using the data_dict['RecordID']
-    bulk_ids.append(data_dict['RecordID'])
+    # loop and query by 0, 999, 1999, etc
 
-# loop and query by 0, 999, 1999, etc
+    missing_chunks = []
+    for z in bulk_ids:
+        try:
+            res = es.get(index="documents", doc_type='article', id=z)
+        except:
+            missing_chunks.append(z)
+    data.append((f, len(bulk_ids), ",".join(bulk_ids)))
 
-missing_chunks = []
-for z in bulk_ids:
-    try:
-        res = es.get(index="documents", doc_type='article', id=z)
-    except:
-        missing_chunks.append(z)
-
-print(len(missing_chunks))        
+with open("elastic_counts.csv", "a") as e:
+    for d in data:
+        e.write("".join([l[0], ',', l[1], ',', l[2], '\n']))
+e.close()        
