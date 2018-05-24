4796cf0635edc6cc4670569eb28bcdf2ab360370
mjlavin80 <lavin@pitt.edu>
2018-05-24 11:36:49 -0400

    subjects to db edits

--- count_subjects.py
+++ count_subjects.py
@@ -9,7 +9,7 @@ c = conn.cursor()
 create_statements = ["CREATE TABLE IF NOT EXISTS source_type_counts (_id integer primary key autoincrement, source_type text, doc_count integer)",
 "CREATE TABLE IF NOT EXISTS object_type_counts (_id integer primary key autoincrement, object_type text, doc_count integer, source_id integer,\
 FOREIGN KEY(source_id) REFERENCES source_type_counts(_id))",
-"CREATE TABLE IF NOT EXISTS periodical_counts (_id integer primary key autoincrement, periodical_id, doc_count integer, source_id integer, object_id integer, \
+"CREATE TABLE IF NOT EXISTS periodical_counts (_id integer primary key autoincrement, aps_id integer, doc_count integer, source_id integer, object_id integer, \
 FOREIGN KEY(source_id) REFERENCES source_type_counts(_id), FOREIGN KEY(object_id) REFERENCES object_type_counts(_id))"]
 
 for u in create_statements:
@@ -71,10 +71,10 @@ for i in t.to_dict()['aggregations']['by_source']['buckets']:
         df = pd.DataFrame(j['by_pub']['buckets'], columns=["key", "doc_count"]) 
         
         #get source and objects ids from db, then add to df
-	objectType_id = c.execute("SELECT _id FROM object_type_counts WHERE object_type=?", (objectType,)).fetchone()[0]
+        objectType_id = c.execute("SELECT _id FROM object_type_counts WHERE object_type=?", (objectType,)).fetchone()[0]
         df['object_id'] = objectType_id    
         df['source_id'] = sourceType_id
         
-        df = df.rename(columns = {"key":"periodical_id"})
+        df = df.rename(columns = {"key":"aps_id"})
         df.to_sql('periodical_counts', con=conn, if_exists='append', index=False)
         conn.commit()
